df70f249f5f73824479254fe21ed26a120971672
Ingo Blechschmidt <iblech@web.de>
2015-07-22 17:57:30 +0200

    Zwischenstand; erreicht 800/1000

--- simple-neural-net.py
+++ simple-neural-net.py
@@ -3,6 +3,11 @@
 
 import numpy as np
 import random
+import pickle
+import time
+
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 class Network():
     def __init__(self, layerSizes):
@@ -49,7 +54,7 @@ class Network():
             nabla_w[-l] = np.dot(delta, acts[-l-1].T)
         return (nabla_b, nabla_w)
 
-    def train(self, samples, batchSize, eta):
+    def train(self, samples, batchSize, eta, progress=None):
         samples = [] + samples
 
         v = self.cost(samples)
@@ -58,6 +63,7 @@ class Network():
             random.shuffle(samples)
 
             batches = [ samples[k:k+batchSize] for k in range(0,len(samples),batchSize) ]
+            print("shuffle")
             for batch in batches:
                 nabla_b = [np.zeros(b.shape) for b in self.biases]
                 nabla_w = [np.zeros(w.shape) for w in self.weights]
@@ -67,27 +73,33 @@ class Network():
                     nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
                     nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
 
+                try:
+                    eta = float(open("eta.txt").read())
+                    print("eta override!")
+                except:
+                    pass
+
                 oldWeights = self.weights
                 oldBiases  = self.biases
                 self.weights = [w-(eta/len(batch))*nw 
                                 for w, nw in zip(self.weights, nabla_w)]
                 self.biases = [b-(eta/len(batch))*nb 
                                for b, nb in zip(self.biases, nabla_b)]
-
-                print(self.feedForward([1,2]))
-
-                newV = self.cost(samples)
-                if True or newV < v:
-                    print("* %f@%f " % (v,eta))
+                newV = self.cost(samples[0:1000])
+                p = 0
+                if progress: p = progress()
+                if newV < v:
+                    print("* %f@%f" % (v,eta), p)
                     v = newV
                     eta = eta * 1.2
-                    eta = 5000
-                    break
                 else:
-                    print("  %f@%f " % (v,eta))
-                    self.weights = oldWeights
-                    self.biases  = oldBiases
+                    print("  %f@%f" % (v,eta), p)
+#                   self.weights = oldWeights
+#                   self.biases  = oldBiases
+                    v = newV
                     eta = eta / 2
+#                   self.weights = [w-np.random.normal(0,0.2,w.shape[0]*w.shape[1]).reshape(w.shape[0],w.shape[1])
+#                               for w, nw in zip(self.weights, nabla_w)]
 
     def cost(self, samples):
         return sum([ self.c(self.feedForward(a),np.array(y)) for a,y in samples ]) / len(samples)
@@ -95,10 +107,12 @@ class Network():
     @classmethod
     def sigma(cls, z):
         return 1/(1+np.exp(-z))
+#       return np.tanh(z)
 
     @classmethod
     def sigmaDeriv(cls, z):
         return cls.sigma(z) * (1 - cls.sigma(z))
+#       return 1 - np.tanh(z)*np.tanh(z)
 
     @classmethod
     def c(cls, a, y):
@@ -107,11 +121,19 @@ class Network():
     def cDeriv(cls, a, y):
         return a - y
 
-net = Network([2,10,10,2])
+net = Network([784,30,10])
 
-samples = []
-for i in range(1000):
-    a = np.random.normal(0,4,2)
-    samples.append((a,a))
+training_data = training_data[0:10000]
+test_data     = test_data[0:1000]
+tr_data = training_data[0:1000]
 
-net.train(samples, 100, 1000000)
+def progress():
+    train_results = [(np.argmax(net.feedForward(x)), np.argmax(y)) for (x, y) in tr_data]
+    test_results = [(np.argmax(net.feedForward(x)), y) for (x, y) in test_data]
+    return sum(int(x == y) for (x, y) in train_results), sum(int(x == y) for (x, y) in test_results)
+
+try:
+    net.train(training_data, 100, 0.001, progress)
+except:
+    print(time.time())
+    pickle.dump((net.weights, net.biases), open("net-%d.pkl" % int(time.time()), "wb"))
